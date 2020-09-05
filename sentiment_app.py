7b86a6400241ec7fb6a1acc919527360a4645e9e
Sarvatra <sarvatra98@gmail.com>
2020-09-05 23:36:31 +0530

    intial

--- sentiment_app.py
+++ sentiment_app.py
@@ -6,16 +6,16 @@ Created on Thu Sep  3 21:28:42 2020
 """
 import pickle
 import streamlit as st
-#filename='Final_model.sav'
-#loaded_model = pickle.load(open(filename, 'rb'))
+filename='Final_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 st.title('Food Review')
 #st.beta_set_page_config(page_title='sample', page_icon=':shark:', layout='wide', initial_sidebar_state='expanded')
 st.subheader('Sentiment Analyzer')
 message=st.text_input('Enter Your Review:')
 if st.button('Check Sentiment'):
-    #k=loaded_model.predict([message])
-    #if(k[0]=='negative'):
-    #    st.success("Negative, thank you for bringing this to our attention. We're sorry you had a bad experience.")
-    #else:
-    #    st.success('Thank you for your valuable feedback')
-    #    st.balloons()
+    k=loaded_model.predict([message])
+    if(k[0]=='negative'):
+        st.success("Negative, thank you for bringing this to our attention. We're sorry you had a bad experience.")
+    else:
+        st.success('Thank you for your valuable feedback')
+        st.balloons()
