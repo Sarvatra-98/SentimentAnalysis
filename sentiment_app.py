ab94e544ce0691bcdfa3ee4f92296f62abdb5fb2
Sarvatra <sarvatra98@gmail.com>
2020-09-06 11:16:53 +0530

    second commit

--- sentiment_app.py
+++ sentiment_app.py
@@ -6,6 +6,29 @@ Created on Thu Sep  3 21:28:42 2020
 """
 import pickle
 import streamlit as st
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/jpg;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('image.jpg')
 filename='Final_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 st.title('Food Review')
@@ -18,4 +41,3 @@ if st.button('Check Sentiment'):
         st.success("Negative, thank you for bringing this to our attention. We're sorry you had a bad experience.")
     else:
         st.success('Positive,Thank you for your valuable feedback')
-        st.balloons()
